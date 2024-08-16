b12f552bbcf1fb11e303424be8fd916776e33b77
atto08 <atto08@naver.com>
2024-08-16 12:56:00 +0900

    initial commit

---
+++ com.sparta.msa-assign.eureka.client.review/src/test/java/com/sparta/msa_assign/eureka/client/review/ReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.msa_assign.eureka.client.review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
